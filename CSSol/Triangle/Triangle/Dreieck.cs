85ac3600d2bc70b72ca55bfe87821201c6a024f7
bmit-maj <jonas.maurer@espas.ch>
2020-09-21 13:54:10 +0200

    created a prototype solution

---
+++ CSSol/Triangle/Triangle/Dreieck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    public class Dreieck
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        private double surface;
+
+        private bool simple;
+
+        public Dreieck(double a, double b)
+            /*
+             * Kreiert ein Dreiecksobjekt aus zwei gegebenen Kathetenlängen eines Rechtwinkligen Dreiecks.
+             */
+        {
+            this.a = a;
+            this.b = b;
+            this.c = Math.Sqrt(a * a + b * b);
+            this.surface = a * b / 2;
+            simple = true;
+        }
+
+        public double A()
+        {
+            return this.a;
+        }
+        public double B()
+        {
+            return this.b;
+        }
+        public double C()
+        {
+            return this.c;
+        }
+        public double Umfang()
+        {
+            return this.a + this.b + this.c;
+        }
+
+        public double Fläche()
+        {
+            return this.surface;
+        }
+
+    }
+}
---
+++ CSSol/Triangle/Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String var;
+            float a, b;
+            Dreieck dreieck;
+            Console.WriteLine("Geben Sie zwei Katheten eines rechtwinkligen Dreiecks an.");
+            do {
+                Console.Write("Kathete a = ");
+                var = Console.ReadLine();
+            } while (!float.TryParse(var, out a));
+            do {
+                Console.Write("Kathete b = ");
+                var = Console.ReadLine();
+            } while (!float.TryParse(var, out b));
+            dreieck = new Dreieck(a, b);
+            Console.WriteLine("Gegebene Kathetenlängen waren {0} und {1}, errechnete Hypotenuse ist:  {2}.", dreieck.A(), dreieck.B(), dreieck.C());
+        }
+    }
+}
